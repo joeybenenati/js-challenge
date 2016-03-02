e1f37e0e94259bacae0f13ab3b45bf61848e10ef
Joey Benenati <joey.benenati@gmail.com>
2016-03-01 19:42:41 -0800

    added modal form content

--- contacts-keeper/client/components/AddButton.jsx
+++ contacts-keeper/client/components/AddButton.jsx
@@ -23,6 +23,34 @@ AddButton = React.createClass({
             <RBS.Modal.Title>Contacts Keeper</RBS.Modal.Title>
           </RBS.Modal.Header>
           <RBS.Modal.Body>
+            <RBS.Row className="show-grid">
+              <RBS.Col xs={5} md={5} sm={5}>
+                <RBS.Input type="text" label="First Name" ref="firstName" /> 
+              </RBS.Col>
+              <RBS.Col xs={2} md={2} sm={2} />
+              <RBS.Col xs={5} md={5} sm={5} >
+                <RBS.Input type="text" label="Last Name" ref="lastName"/>
+              </RBS.Col>
+            </RBS.Row>
+            <RBS.Row className="show-grid">
+              <RBS.Col xs={5} md={5} sm={5}>
+                <RBS.Input type="date" label="Date of Birth" ref="dob"/> 
+              </RBS.Col>
+              <RBS.Col xs={2} md={2} sm={2} />
+              <RBS.Col xs={5} md={5} sm={5} >
+                <RBS.Input type="text" label="Phone Number" ref="phone"/>
+              </RBS.Col>
+            </RBS.Row>
+            <RBS.Row className="show-grid">
+              <RBS.Col xs={5} md={5} sm={5}>
+                <RBS.Input type="email" label="Email" ref="email" /> 
+              </RBS.Col>
+            </RBS.Row>
+            <RBS.Row className="show-grid">
+              <RBS.Col xs={12} md={12} sm={12}>
+                <RBS.Input type="textarea" label="Notes" ref="notes" /> 
+              </RBS.Col>
+            </RBS.Row>
           </RBS.Modal.Body>
           <RBS.Modal.Footer>
             <RBS.Button className='btn-save'>Save</RBS.Button>
