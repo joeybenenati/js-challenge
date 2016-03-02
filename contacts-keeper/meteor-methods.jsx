b3c2a52bb6650b5cb1be13d7cd84bf996f3b33c9
Joey Benenati <joey.benenati@gmail.com>
2016-03-01 19:22:32 -0800

    added addContact db  method to meteor methods

--- contacts-keeper/meteor-methods.jsx
+++ contacts-keeper/meteor-methods.jsx
@@ -1,3 +1,11 @@
 Meteor.methods({
-  //addContact method
+  
+  addContact(contact) {
+    return Contacts.insert(contact, err => {
+      if (err) {
+        console.log('failed to add contact: ', err.message)
+      }
+    })
+  }
+
 })
