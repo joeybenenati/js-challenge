0ba6891ce0ee922e502ecf9685468edd5dce2b32
Joey Benenati <joey.benenati@gmail.com>
2016-03-01 18:26:56 -0800

    - added contacts collection - app header - search input field

---
+++ contacts-keeper/client/App.jsx
@@ -0,0 +1,25 @@
+App = React.createClass({
+
+  render() {
+    return (
+      <div>
+        <RBS.PageHeader><small>Contacts Keeper</small></RBS.PageHeader>
+        <div className='content'>
+          <RBS.Row className="show-grid controls">
+            <RBS.Col xs={5} sm={4} md={4}>
+              <div className="input-group">
+                <input type="text" className="form-control" placeholder="Search"/>
+                <div className="input-group-addon"><RBS.Glyphicon glyph="search" /></div>
+              </div>
+            </RBS.Col>
+            <RBS.Col xs={4} sm={6} md={6}></RBS.Col>
+            <RBS.Col xs={3} sm={2} md={2} >
+              <RBS.Button bsStyle='primary' />
+            </RBS.Col>
+          </RBS.Row>
+        </div>
+      </div>
+
+    )
+  }
+})
